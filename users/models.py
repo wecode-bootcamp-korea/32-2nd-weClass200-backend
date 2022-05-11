ef6cded889455ff209ca096fa7a0ae19e4f51f74
NamkyuAhn <mkdir.nkahn@gmail.com>
2022-05-11 17:36:39 +0900

    Edit : models

--- users/models.py
+++ users/models.py
@@ -4,11 +4,11 @@ from django.db import models
 class User(models.Model):
     name          = models.CharField(max_length=30)
     kakao_id      = models.BigIntegerField(default=0, null=True)
-    email         = models.CharField(max_length=200)
+    email         = models.CharField(max_length=200, null=True)
     phone_number  = models.CharField(max_length=50, null=True)
-    profile_image = models.CharField(max_length=500)
-    gender        = models.CharField(max_length=30)
-    age           = models.PositiveIntegerField()
+    profile_image = models.CharField(max_length=500, null=True)
+    gender        = models.CharField(max_length=30, null=True)
+    age           = models.PositiveIntegerField(null=True)
     myclasses     = models.ManyToManyField('products.Product', through="users.MyClass", related_name='user_myclass')
     reviews       = models.ManyToManyField('products.Product', through="users.Review", related_name='user_review')
     likes         = models.ManyToManyField('products.Product', through="users.Like", related_name='user_like')
@@ -37,14 +37,16 @@ class MyClass(models.Model):
 
 class Like(models.Model):
     user    = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    product = models.ForeignKey('products.Product', on_delete=models.SET_NULL, null=True)
+    product = models.ForeignKey('products.Product', on_delete=models.SET_NULL, 
+                                 null=True, related_name='product_like')
 
     class Meta:
         db_table = 'likes'
 
 class Review(models.Model):
     user         = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    product      = models.ForeignKey('products.Product', on_delete=models.SET_NULL, null=True)
+    product      = models.ForeignKey('products.Product', on_delete=models.SET_NULL, 
+                                      null=True, related_name='product_review')
     score        = models.PositiveIntegerField()
     detail       = models.CharField(max_length=1000)
     created_at   = models.DateTimeField(auto_now_add=True)
@@ -54,7 +56,8 @@ class Review(models.Model):
         db_table = 'reviews'
 
 class ReviewImage(models.Model):
-    review     = models.ForeignKey(Review, on_delete=models.SET_NULL, null=True)
+    review     = models.ForeignKey(Review, on_delete=models.SET_NULL, 
+                                   null=True, related_name='images')
     image_urls = models.CharField(max_length=500, null=True, default=None)
 
     class Meta:
