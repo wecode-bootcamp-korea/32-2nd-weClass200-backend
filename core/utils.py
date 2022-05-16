686004addd924fed38f23a3b1b80bed54f4a1c29
BDjaekwanee <jaekwanee@gmail.com>
2022-05-16 15:16:48 +0900

    Feature :Create login_decorateor

---
+++ core/utils.py
@@ -0,0 +1,28 @@
+import jwt
+
+from users.models import User
+from django.http  import JsonResponse
+from django.conf  import settings
+
+def login_decorator(func) :
+    def wrapper(self, request, *args, **kwrags) :
+        try :
+            new_token = request.headers.get('Authorization', None)
+            payload = jwt.decode(new_token, settings.SECRET_KEY, settings.ALGORITHM)
+            request.user = User.objects.get(id=payload['id'])
+
+        except User.DoesNotExist:
+            return JsonResponse({'MESSAGE' : 'INVALID_USER'}, status=401)
+
+        except jwt.exceptions.DecodeError :
+            return JsonResponse({'MESSAGE': 'INVALID_TOKEN'}, status=401)
+
+        except jwt.ExpiredSignatureError :
+            return JsonResponse({'message':'EXPIRED_TOKEN'}, status=401)
+
+        except jwt.InvalidSignatureError:
+            return JsonResponse({'message' : 'invalid_signature'}, status=401)
+
+        return func(self, request, *args, **kwrags)
+
+    return wrapper
