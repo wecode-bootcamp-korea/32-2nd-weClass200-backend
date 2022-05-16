7e3c5da876d4ecbe89a2a3aa78c03baa603cab4b
BDjaekwanee <jaekwanee@gmail.com>
2022-05-16 14:56:10 +0900

    refactor:Fix KakaoLoginView

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,83 @@
-from django.test import TestCase
+import jwt
 
-# Create your tests here.
+from django.test         import TestCase, Client
+from django.conf         import settings
+
+from unittest.mock       import patch
+from users.models        import User
+from django.conf         import settings
+
+class KakaoSignTest(TestCase):
+    def setUp(self):
+        User.objects.create(
+            id       = 1,
+            name     = "재관",
+            kakao_id = 2237020088,
+            email    = "cyc04088@naver.com"
+        )
+    
+    def tearDown(self):
+        User.objects.all().delete()
+        
+    @patch("users.views.requests.get")
+    def test_kakao_signin_succes(self, mocked_kakao_user_info):
+        client = Client()
+
+        class MockedResponse:
+            def json(self):
+                return {
+    "id": 2237020088,
+    "properties": {
+        "nickname": "재관"
+    },
+    "kakao_account": {
+        "profile": {
+            "nickname": "재관",
+            "profile_image_url": "http://k.kakaocdn.net/dn/dpk9l1/btqmGhA2lKL/Oz0wDuJn1YV2DIn92f6DVK/img_640x640.jpg",
+        },
+        "has_email": True,
+        "email": "cyc04088@naver.com",
+        "gender": "male"
+    }
+}
+
+        mocked_kakao_user_info.return_value = MockedResponse()
+
+        headers  = {"Authorization": "가짜 access_token"}
+        response = client.get("/users/kakao", **headers)
+        
+        user_id = jwt.decode(response.json()['new_token'], settings.SECRET_KEY, settings.ALGORITHM)
+        new_token   = jwt.encode({'id':user_id['id']}, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {"message" : "REGISTERED_USER", "new_token" : new_token})
+        
+    @patch("users.views.requests.get")
+    def test_kakao_signup_success(self, mocked_kakao_user_info):
+        client = Client()
+
+        class MockedResponse:
+            def json(self):
+                return {
+    "id": 123456789,
+    "properties": {
+        "nickname": "남규"
+    },
+    "kakao_account": {
+        "profile": {
+            "nickname": "남규",
+            "profile_image_url": "http://k.kakaocdn.net/dn/dpk9l1/btqmGhA2lKL/Oz0wDuJn1YV2DIn92f6DVK/img_640x640.jpg",
+        },
+        "gender": "male",
+        "email" : "cyc04088@naver.com",
+    }
+}
+
+        mocked_kakao_user_info.return_value = MockedResponse()
+
+        headers  = {"Authorization": "가짜 access_token"}
+        response = client.get("/users/kakao", **headers)
+        
+        user_id = jwt.decode(response.json()['new_token'], settings.SECRET_KEY, settings.ALGORITHM)
+        new_token   = jwt.encode({'id':user_id['id']}, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.json(), {"message" : "NEW_USER", "new_token" : new_token})
--- users/views.py
+++ users/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+import jwt, requests
 
-# Create your views here.
+from django.views     import View
+from django.http      import JsonResponse
+from django.conf      import settings
+
+from users.models     import User
+        
+def get_kakao_user_information(access_token):
+    header = {
+        "Authorization" : f'Bearer {access_token}',
+        "Content-type"  : "application/x-www-form-unlencoded;charset=utf-8"
+    }
+    kakao_user_info = requests.get("https://kapi.kakao.com/v2/user/me", headers = header, timeout = 2)
+
+    if not kakao_user_info.status_code == 200: 
+        return False
+
+    return kakao_user_info.json()        
+
+class KakaoLoginView(View):
+    def get(self, request):
+        new_token       = request.headers.get('Authorization', None)
+        kakao_user_info = get_kakao_user_information(new_token)
+
+        if not kakao_user_info:
+            return JsonResponse({"message" : "INVALID_KAKAO_TOKEN"}, status = 401)
+            
+        kakao_id        = kakao_user_info['id']
+        nickname        = kakao_user_info['kakao_account']['profile']['nickname']
+        profile_image   = kakao_user_info['kakao_account']['profile']['profile_image_url']
+        email           = kakao_user_info['kakao_account']['email']
+        
+
+        user, created = User.objects.get_or_create(
+            kakao_id = kakao_id, 
+            defaults = {'name' : nickname, 'email' : email, 'profile_image' :profile_image}
+            )   
+        new_token = jwt.encode({'id':user.id}, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+
+        if created:
+            return JsonResponse({"message" : "NEW_USER", "new_token" : new_token}, status = 201)
+        
+        return JsonResponse({"message" : "REGISTERED_USER", "new_token" : new_token}, status = 200)
